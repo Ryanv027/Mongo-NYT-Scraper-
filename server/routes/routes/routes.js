8e2699c51025f98bb53307706d8b72c70e13364c
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-06-25 18:50:07 -0700

    procfile

--- server/routes/routes/routes.js
+++ server/routes/routes/routes.js
@@ -5,6 +5,6 @@ const articleScraper = require("./../../models/articlesModel");
 module.exports = app => {
   app.get("/", (req, res) => {
     console.log("hit router");
-    res.sendFile(path.join(__dirname + "./../public/index.html"));
+    res.sendFile(path.join(__dirname + "./../build/index.html"));
   });
 };
